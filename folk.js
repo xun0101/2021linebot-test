f855ba37b1ff3a9b01c2f112fba7545c592d4b47
xun0101 <dabby6521@gmail.com>
2021-12-03 00:11:01 +0800

    待確認

--- folk.js
+++ folk.js
@@ -6,38 +6,36 @@ export default async (event) => {
   try {
     if (event.message.type === 'text') {
       let newflex = JSON.parse(JSON.stringify(flex))
-      console.log(flex)
-      console.log(newflex)
       const replies = []
       let a = 0
       for (let i = 0; i < data.length; i++) {
         if (data[i].caseName === event.message.text) {
           newflex.contents.header.contents.text = data[i].caseName
           newflex.contents.hero.url = data[i].representImage
-          newflex.contents.body.contents[0].action.text = (`${data[i].caseName}活動介紹`)
-          newflex.contents.body.contents[1].action.text = (`${data[i].caseName}特點`)
+          newflex.contents.body.contents[0].action.text = (`${data[i].caseName}特點`)
+          newflex.contents.body.contents[1].action.text = (`${data[i].caseName}地址`)
           a = 1
-        } else if ((`${data[i].caseName}歷史`) === event.message.text) {
-          replies.push(`名俗由來:\n${data[i].historyDevelopment}`)
-        // } else if ((data[i].govInstitutionName || data[i].belongCity) === event.message.text) {
-        //   replies.push(`特有名俗:\n${data[i].caseName}`)
         } else if ((`${data[i].caseName}地址`) === event.message.text) {
           replies.push(`地址:\n${data[i].hostContactorAddress}`)
         } else if ((`${data[i].caseName}特點`) === event.message.text) {
           replies.push(`活動特點:\n${data[i].ceremonyFeature}\n名俗特點:\n${data[i].judgeCriteria.join('\n')}`)
-        } else if ((`${data[i].caseName}活動介紹`) === event.message.text) {
-          replies.push(`活動介紹:\n${data[i].registerReason}}`)
-        }
+        }// } else if ((data[i].govInstitutionName || data[i].belongCity) === event.message.text) {
+        //   replies.push(`特有名俗:\n${data[i].caseName}`)
+        // else if ((`${data[i].caseName}活動介紹`) === event.message.text) {
+        //   replies.push(`活動介紹:\n${data[i].registerReason}}`)
+        // }
       }
       for (let j = 0; j < data2.length; j++) {
         if (data2[j].actName === event.message.text) {
-          replies.push(`活動說明:\n${data2[j].description}`)
+          newflex.contents.header.contents.text = data2[j].actName
+          newflex.contents.hero.url = data2[j].representImage
+          newflex.contents.body.contents[0].action.text = (`${data2[j].actName}詳情`)
+          newflex.contents.body.contents[1].action.text = (`${data2[j].actName}地址`)
+          a = 1
         } else if ((`${data2[j].actName}詳情`) === event.message.text) {
-          replies.push(`活動詳情:\n${data2[j].website}`)
+          replies.push(`活動現況:\n${data2[j].cycle}\n活動說明:\n${data2[j].description}\n活動網址:\n${data2[j].website}`)
         } else if ((`${data2[j].actName}地址`) === event.message.text) {
           replies.push(`活動地址:\n${data2[j].address}`)
-        } else if ((`${data2[j].actName}現況`) === event.message.text) {
-          replies.push(`活動現況:\n${data2[j].cycle}`)
         }
       }
       if (replies.length > 0) {
