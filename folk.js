22aa94f16aea2df0800ca1b5205a2fbdaff35584
Link1515 <h26281660@gmail.com>
2021-12-03 10:50:42 +0800

    fix flex img bug

--- folk.js
+++ folk.js
@@ -5,19 +5,21 @@ import flex from './flex.js'
 export default async (event) => {
   try {
     if (event.message.type === 'text') {
-      let newflex = JSON.parse(JSON.stringify(flex))
+      const newflex = JSON.parse(JSON.stringify(flex))
       const replies = []
       let a = 0
+
       for (let i = 0; i < data.length; i++) {
         if (data[i].caseName === event.message.text) {
           newflex.contents.header.contents.text = data[i].caseName
           newflex.contents.hero.url = data[i].representImage
-          newflex.contents.body.contents[0].action.text = (`${data[i].caseName}特點`)
-          newflex.contents.body.contents[1].action.text = (`${data[i].caseName}地址`)
+          newflex.contents.body.contents[0].action.text = `${data[i].caseName}特點`
+          newflex.contents.body.contents[1].action.text = `${data[i].caseName}地址`
           a = 1
-        } else if ((`${data[i].caseName}地址`) === event.message.text) {
+          break
+        } else if (`${data[i].caseName}地址` === event.message.text) {
           replies.push(`地址:\n${data[i].hostContactorAddress}`)
-        } else if ((`${data[i].caseName}特點`) === event.message.text) {
+        } else if (`${data[i].caseName}特點` === event.message.text) {
           replies.push(`活動特點:\n${data[i].ceremonyFeature}\n名俗特點:\n${data[i].judgeCriteria.join('\n')}`)
         } else if (data[i].belongCity === event.message.text) {
           replies.push(`特有名俗:\n${data[i].caseName}\n活動介紹:\n${data[i].registerReason}}`)
@@ -26,17 +28,20 @@ export default async (event) => {
       for (let j = 0; j < data2.length; j++) {
         if (data2[j].actName === event.message.text) {
           newflex.contents.header.contents.text = data2[j].actName
-          newflex.contents.hero.url = data2[j].imageUrl
-          newflex.contents.body.contents[0].action.text = (`${data2[j].actName}詳情`)
-          newflex.contents.body.contents[1].action.text = (`${data2[j].actName}地址`)
+          newflex.contents.hero.url = 'https://cloud.culture.tw/' + data2[j].imageUrl
+          newflex.contents.body.contents[0].action.text = `${data2[j].actName}詳情`
+          newflex.contents.body.contents[1].action.text = `${data2[j].actName}地址`
           a = 1
-          console.log('123')
-        } else if ((`${data2[j].actName}詳情`) === event.message.text) {
-          replies.push(`活動現況:\n${data2[j].cycle}\n活動說明:\n${data2[j].description}\n活動網址:\n${data2[j].website}`)
-        } else if ((`${data2[j].actName}地址`) === event.message.text) {
+          break
+        } else if (`${data2[j].actName}詳情` === event.message.text) {
+          replies.push(
+            `活動現況:\n${data2[j].cycle}\n活動說明:\n${data2[j].description}\n活動網址:\n${data2[j].website}`
+          )
+        } else if (`${data2[j].actName}地址` === event.message.text) {
           replies.push(`活動地址:\n${data2[j].address}`)
         }
       }
+
       if (replies.length > 0) {
         event.reply(replies.slice(0, 5))
         console.log(replies)
