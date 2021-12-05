e1f01d4f6b9676dc60e78a01c1792d9ac686861d
xun0101 <dabby6521@gmail.com>
2021-12-05 14:03:15 +0800

    05

--- folk.js
+++ folk.js
@@ -58,7 +58,7 @@ export default async (event) => {
           while (numbers.length < 3) {
             console.log(789)
             const num = rand(1, data2.length)
-            if (!numbers.includes(num)) {
+            if (!numbers.includes(data2[num])) {
               // 1204
               console.log(data2[num].imageUrl)
               numbers.push({
@@ -73,13 +73,13 @@ export default async (event) => {
           }
           newflex2.contents.contents[0].hero.url = 'https://cloud.culture.tw/' + numbers[0].img
           newflex2.contents.contents[0].body.contents.text = numbers[0].act
-          // newflex2.contents.contents[0].body.contents.action.uri = numbers[0].web
+          newflex2.contents.contents[0].body.contents.action.uri = numbers[0].web
           newflex2.contents.contents[1].hero.url = 'https://cloud.culture.tw/' + numbers[1].img
           newflex2.contents.contents[1].body.contents.text = numbers[1].act
-          // newflex2.contents.contents[1].body.contents.action.uri = numbers[1].web
+          newflex2.contents.contents[1].body.contents.action.uri = numbers[1].web
           newflex2.contents.contents[2].hero.url = 'https://cloud.culture.tw/' + numbers[2].img
           newflex2.contents.contents[2].body.contents.text = numbers[2].act
-          // newflex2.contents.contents[2].body.contents.action.uri = numbers[2].web
+          newflex2.contents.contents[2].body.contents.action.uri = numbers[2].web
         }
       }
 
